34016a861bcd0316eae9f7fee3a3c43361ada8be
Samuraien2 <samuraien2@proton.me>
2025-12-09 13:39:42 +0100

    removed comments from boot.s because assembly is so readable

--- src/boot.s
+++ src/boot.s
@@ -1,17 +1,12 @@
-/* boot.s - Simple ARM Bootloader */
 .global _start
 .section .text
 _start:
-    /* Set up the stack pointer */
     ldr sp, =stack_top
-
-    /* Jump to kernel main function */
     bl kernel_main
 
 halt:
     b halt
 
-/* Define stack */
 .section .bss
 .align 3
 stack:
